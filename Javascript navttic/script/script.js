9089ab1e85a3ec8020e2085f00ee1c18439e1c4d
Faisal-Hayat-11 <faisalhayat511@gmail.com>
2026-03-05 15:48:11 +0500

    i add a new folder where i did js code

---
+++ Javascript navttic/script/script.js
@@ -0,0 +1,152 @@
+// console.log("Welcome to my world")
+
+
+// document.writeln("witness My Greatness")
+
+// let num2 = document.writeln('whats up')
+
+// let num3 = Number(Number(prompt("Enter first value"))
+// le)t dic = Number(prompt("Enter / + , -, *, %")
+// l)et num4 = Number(Number(prompt("Enter 2nd value"))
+// let )res = num3 / num4
+// // let eve = 
+
+
+// if(dic == "*") {
+//  console.log(num3 * num4)
+ 
+ 
+ 
+// }
+// else if(dic == "-") {
+//     console.log(num3 - num4)
+// }
+// else if(dic == "+") {
+//     console.log(num3 + num4)
+// }
+// else if(dic == "/"){
+//     console.log(num3 / num4)
+// }
+// else if(dic == "%"){
+//     let res = num3/num4
+//     // console.log(num3 / num4 === 0)
+//     if(res % 2 == 0){
+//     console.log("Even")
+//  }
+//  else{
+//     console.log("Odd")
+//  }
+// }
+// else{
+//     console.log("Enter correct dic")
+// }
+
+// console.log("program after devision")
+
+// console.log(true&&false);
+
+
+// let num = 10;
+// let num2 = 5;
+// let sw;
+
+// sw = num;
+// num = num2;
+// num2 = sw;
+
+// console.log("num= ",num, "num2 =",num2)
+
+// let 
+
+// const employee = {
+//     banner_img : "image",
+//     user_img : "https://www.pinterest.com/pin/47217496092635143/",
+//     name : "Faisal Hayat",
+//     icon : "image",
+//     rating : "3rd",
+//     sdescrption : "Peshawar, Hayatbad, Pakistan",
+//     contact_button : "Contact info",
+//     connection_count : 184,
+//     connection : "Connection",
+//     btn_icon : "https://www.pinterest.com/pin/47217496092635143/",
+//     Mbtn : "Message",
+//     follow_btn_icon : "image",
+//     Fbtn : "follow",
+//     More_btn : "More",
+//     sponsor_logo : "image"
+
+// };
+
+// document.writeln(`${employee.banner_img} <br> ${employee.user_img} <br> <b>${employee.name}</b> <br> ${employee.icon} <br> <b>${employee.rating}</b> <br> <b>${employee.sdescrption}</b> <br> ${employee.contact_button} <br> ${employee.connection} <br> ${employee.btn_icon} <br> <b>${employee.Mbtn}</b> <br> ${employee.follow_btn_icon} <br> <em><b>${employee.Fbtn} <br> ${employee.More_btn}</b> <br> ${employee.sponsor_logo}`)
+
+// document.writeln(`<table border=2px><tr> <th style="padding: 10px;">Name</th> <th>user_img</th> <th>sdescrption</th> <th>contact_button</th> </tr>
+//     <tr>
+//     <td style="color: red; background-color: blue; padding: 10px"><b>${employee.name}</td>
+//     <td><b>${employee.user_img}</td>
+//     <td><b>${employee.sdescrption}</td>
+//     <td><b>${employee.contact_button}</td>
+
+//     </tr></table>`)
+
+
+
+// const subject_1= Number(prompt("Enter your subject Marks"));
+// const subject_2= Number(prompt("Enter your subject Marks"));
+// const subject_3= Number(prompt("Enter your subject Marks"));
+// // const subject_4= Number(prompt("Enter your subject Marks"));
+// // const subject_5= Number(prompt("Enter your subject Marks"));
+// // const subject_6= Number(prompt("Enter your subject Marks"));
+// // const subject_7= Number(prompt("Enter your subject Marks"));
+// // const subject_8= Number(prompt("Enter your subject Marks"));
+
+// let marks = subject_1 + subject_2 + subject_3;
+// console.log(marks)
+
+// if(marks >= 700 || marks === 800){
+//     console.log("Youre Grade is A+");
+// }
+// else if(marks >= 600 || marks === 699){
+//     console.log("Youre Grade is A");
+// }
+// else if(marks >= 500 || marks === 599){
+//     console.log("Youre Grade is B");
+// }
+// else if(marks >= 400 || marks === 499){
+//     console.log("Youre Grade is C");
+// }
+// else if(marks >= 300 || marks === 399){
+//     console.log("Youre Grade is D");
+// }
+// else{
+//     console.log("Youre Fail");
+// }
+
+// let avg = marks/800 *100;
+// console.log("Youre Avg is", avg);
+
+// let age = 19;
+
+// switch(age) {
+//     case 18:
+//         console.log("youre 12 adult");
+//         console.log("youre adult");
+//         console.log("youre adult");
+//         console.log("youre adult");
+//     break;
+//     case 19:
+//         console.log("youre adult");
+//         console.log("youre adult");
+//         console.log("youre adult");
+//     break;
+// }
+
+let age = 10;
+if(age > 18 ){
+    console.log("youre adult");
+}
+else if(age < 18){
+    console.log("Youre minor");
+}
+else{
+    console.log("youre Dead");
+}
